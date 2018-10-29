edbc5739c79987214927d1e8c761b12585d9ebc9
Three Eyed Venelope <threeeyedvenelope@gmail.com>
2018-10-29 14:25:41 -0500

    Add scene change

--- 2D Side Scroller Video Demo/Assets/Scripts/Door.cs
+++ 2D Side Scroller Video Demo/Assets/Scripts/Door.cs
@@ -1,17 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Door : MonoBehaviour {
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneToLoad;
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private bool isPlayerInTrigger;
+
+    // Triggers the door twice because the player has two colliders
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Player"))
+    //    {
+    //        if (Input.GetButtonDown("Activate"))
+    //        {
+    //            Debug.Log("Player activated door!");
+    //        }
+    //    }
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            if (Input.GetButtonDown("Activate"))
-            {
-                Debug.Log("Player activated door!");
-            }
+            isPlayerInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Activate") && isPlayerInTrigger)
+        {
+            Debug.Log("Player activated door!");
+            SceneManager.LoadScene(sceneToLoad);
         }
     }
 }
+
+
