57245b8322bbd866d040eaf2b64a463656a51cd5
Three Eyed Venelope <threeeyedvenelope@gmail.com>
2018-12-02 21:17:12 -0600

    Add sound effect

--- 2D Side Scroller Video Demo/Assets/Scripts/Collect.cs
+++ 2D Side Scroller Video Demo/Assets/Scripts/Collect.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class Collect : MonoBehaviour
 {
+    private AudioSource audioSource;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
+    private static int coinCount = 0;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Destroy(gameObject);
+            coinCount++;
+            Debug.Log("Coin count: " + coinCount);
+            audioSource.Play();
+            spriteRenderer.enabled = false;
+            boxCollider2D.enabled = false;
+            Destroy(gameObject, audioSource.clip.length);
         }
     }
 }
